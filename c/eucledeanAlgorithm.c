a4aed04891cc60a17763a81fddb1e2fa1fc8f22e
Olivia Polos <38380599+odp31@users.noreply.github.com>
2024-11-23 09:29:46 -0500

    Create eucledeanAlgorithm.c

---
+++ c/eucledeanAlgorithm.c
@@ -0,0 +1,21 @@
+int gcd(int a, int b)
+{
+  if (b == 0) {
+    return a;
+  }
+  else {
+    return gcd(b, a % b);
+  }
+}
+
+int main()
+{
+  int a, b;
+  printf("enter two integers; ");
+  scanf("%d%d", &a, &b);
+
+  int result = gcd(a, b);
+  printf("GCD of %d and %d is %d\n", a, b, result);
+
+  return 0;
+}
